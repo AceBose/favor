d0884110d97befb83d432873b9929f58662b6b08
Eric <enewcomer23@gmail.com>
2018-02-03 19:40:31 -0800

    removed useless css

--- favorapp/views/index.py
+++ favorapp/views/index.py
@@ -1,8 +1,8 @@
 from flask import render_template, Blueprint
 
-bp = Blueprint(__name__, __name__,template_folder='templates')
+bp = Blueprint(__name__, __name__, template_folder='templates')
+
 
 @bp.route('/')
 def show():
-       # return "Hello World"
-        return render_template('user.html')
+    return render_template('user.html')
